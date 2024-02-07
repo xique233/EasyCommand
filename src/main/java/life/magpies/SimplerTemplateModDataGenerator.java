645a4ffdf2b73448a382f215d98f332100301410
Magpie <320718257@qq.com>
2024-02-07 02:14:22 -0400

    Initial commit

---
+++ src/main/java/life/magpies/SimplerTemplateModDataGenerator.java
@@ -0,0 +1,11 @@
+package life.magpies;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimplerTemplateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
